283f7b6aa394ee3855174b344841b9d62567049b
=xianyunyehe <tianlei542475736@gmail.com>
2021-06-06 23:45:04 +0800

    add img preview

--- frontend/src/components/Result.vue
+++ frontend/src/components/Result.vue
@@ -8,15 +8,34 @@
 
 <template>
   <div class="result">
-    <progressive-img
+    <!-- <progressive-img
       class="img"
       :blur="3"
       :src="imgSrc"
       :placeholder="baseURL + 'example.gif'"
       :fallback="baseURL + 'error.gif'"
       alt="Your O'RLY Book Cover"
-    ></progressive-img>
+    ></progressive-img> -->
+    
+    <div class="pic-area">
+      <div class='pic-top'></div>
+      <div class="top-text">
+        {{top_text}}
+      </div>
+      <div class="img-box"> <img src="/img/0.tif.56a5ab37.gif" alt=""></div>
+      <div class="main-title">
+        {{main_title}}
+      </div>
+      <div class="extra-title">
+        {{extra_title}}
+      </div>
+      <div class="foot">
+        <div class="foot-left">O'RLY?</div>
+        <div class="author-text">{{author}}</div>
+      </div>
+    </div>
     <a :href="imgSrc" target="_blank" download>{{$t("download")}}</a>
+
   </div>
 </template>
 
@@ -33,6 +52,11 @@
       return {
         baseURL: process.env.BASE_URL,
         imgSrc: "",
+        top_text: "Top Text 顶部文字",
+        main_title: "Title 标题",
+        extra_title: "Guid Title副标题",
+        foot_left_text: "O'RLY?",
+        author: "Author 作者"
       }
     },
     watch: {
@@ -49,6 +73,56 @@
 <style scoped>
   .result {
     width: 400px;
+  }
+  .img-box {
+    margin-top: 10px;
+    padding-left: 70px;
+  }
+  .pic-top {
+    margin: 0 auto;
+    width: 360px;
+    background: #888888;
+    height: 8px;
+  }
+  .main-title {
+    width: 360px;
+    height: 110px;
+    margin: 0 auto;
+    background: #777777;
+    text-align: left;
+    line-height: 110px;
+    font-size: 56px;
+    color: #fff;
+    padding-left: 10px;
+    font-weight: bold;
+  }
+  .extra-title{
+    text-align: right;
+    font-weight: bold;
+    font-size: 17px;
+    margin: 5px  18px 0 0 ;
+
+
+  }
+  .pic-area {
+    width: 400px;
+    height: 560px;
+    box-shadow: 10px 10px 20px #999;
+  }
+  .pic-area img {
+    width: 280px;
+    height: 280px;
+    margin-left: 30px;
+  }
+  .foot{
+    display: flex;
+    justify-content: space-between;
+    margin-top: 60px;
+    padding: 0 25px;
+  }
+  .author-text{
+    font-size: 13px;
+    font-weight: bold;
   }
   @media screen and (min-width: 970px) {
     .result {
